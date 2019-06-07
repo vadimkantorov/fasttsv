3f9605b7ea6104566d33e62ac3a01c2a725c1eb9
Vadim Kantorov <vadimkantorov@gmail.com>
2019-06-07 15:13:38 +0200

    Integers (non-negative) work well

--- fasttsv.py
+++ fasttsv.py
@@ -12,8 +12,8 @@ def loads(b, max_integer_width = 4, encoding = 'utf-8', delimiter = '\t', newlin
 
 	integer_cols = [(i, n) for i, (n, t) in enumerate(head.dtype.descr) if np.issubdtype(np.dtype(t), np.integer)] if not uniform else np.arange(head.shape[-1], dtype = int) if np.issubdtype(head.dtype, np.integer) else np.array([], dtype = int)
 	float_cols = [(i, n) for i, (n, t) in enumerate(head.dtype.descr) if np.issubdtype(np.dtype(t), np.floating)] if not uniform else np.arange(head.shape[-1], dtype = int) if np.issubdtype(head.dtype, np.floating) else np.array([], dtype = int)
-	integers = np.array(list(zip(*integer_cols))[0]) if not uniform else integer_cols
-	floats = np.array(list(zip(*float_cols))[0]) if not uniform else float_cols
+	integers = np.array(list(zip(*integer_cols))[0]) if not uniform else slice(None)
+	floats = np.array(list(zip(*float_cols))[0]) if not uniform else slice(None)
 
 	a = np.frombuffer(b, dtype = np.uint8)
 
@@ -27,27 +27,28 @@ def loads(b, max_integer_width = 4, encoding = 'utf-8', delimiter = '\t', newlin
 	minus = a == ord(b'-')
 	num_rows = newlines.sum()
 
-	a0 = np.zeros((max_integer_width - 1 + len(a), ), dtype = np.uint8)
+	a0 = np.empty((max_integer_width - 1 + len(a), ), dtype = np.uint8)
+	a0[:max_integer_width - 1].fill(0)
 	np.subtract(a, ord(b'0'), out = a0[max_integer_width - 1:])
 	m = as_strided(a0, shape = [max_integer_width, len(a)], strides = a0.strides * 2)[::-1]
 	m = m * np.power(10, np.arange(max_integer_width), dtype = np.int32)[:, None]
 	for i in range(1, max_integer_width):
 		np.add(m[i], m[i - 1], out = m[i])
 
-	breaksi = np.flatnonzero(tabs | newlines)
-
+	breaksi = np.flatnonzero(np.bitwise_or(tabs, newlines, out = tabs)).astype(np.int32).reshape(num_rows, -1)
+	breaksi -= 1
 	if len(integer_cols) > 0:
-		widthi = np.diff(np.pad(breaksi, (1, 0), mode = 'constant', constant_values = -1))
-		BI = (breaksi.reshape(num_rows, -1) - 1)[:, integers].flatten()
-		WI = (widthi.reshape(num_rows, -1) - 2)[:, integers].flatten()
-		resi = m[WI, BI].reshape(num_rows, -1)
+		widthi = np.diff(breaksi.ravel(), prepend = -2).reshape(num_rows, -1)
+		widthi -= 2
+		resi = m[widthi[:, integers], breaksi[:, integers]].reshape(num_rows, -1)
 
-	if len(floats) > 0:
+	if len(float_cols) > 0:
 		points = a == ord(b'.')
 		breaksf = np.flatnonzero(points)
-		BF = (breaksi.reshape(num_rows, -1) - 1)[:, floats].flatten()
+		BF = breaksi[:, floats].flatten()
 		BF_ = np.vstack([breaksf - 1, BF]).T.flatten()
-		BF__ = (breaksi.reshape(num_rows, -1))[:, floats - 1].flatten()
+		breaksi += 1
+		BF__ = breaksi[:, floats - 1].flatten()
 		widthf = BF - breaksf
 		WF_ = np.vstack([breaksf - BF__ - 1, widthf]).T.flatten() - 1
 		resf = m[WF_, BF_].reshape(num_rows, -1)
@@ -60,10 +61,10 @@ def loads(b, max_integer_width = 4, encoding = 'utf-8', delimiter = '\t', newlin
 		float_cols = {n : j for j, (i, n) in enumerate(float_cols)}
 		return np.rec.fromarrays([resi[:, integer_cols[n]] if n in integer_cols else resf[:, float_cols[n]] for n in head.dtype.names], names=head.dtype.names)
 	else:
-		return resi if len(integers) > 0 else resf
+		return resi if len(integer_cols) > 0 else resf
 		
 if __name__ == '__main__':
-	# python3 -m cProfile -s tottime tsv.py
+	# python3 -m cProfile -s cumtime fasttsv.py
 
 	tic = time.time()
 	#print(loads(b'# a\tb\tc\td\n1\t22.60\t3\t5.0\n3\t44.8\t8\t9.09\n'))
@@ -75,8 +76,8 @@ if __name__ == '__main__':
 	# baseline
 	#res = np.loadtxt(io.StringIO(b.decode('ascii')), dtype = np.float32 if floats else int, delimiter = '\t')
 
-	print(loads(open('test.txt', 'rb').read()).shape)
-	#M = loads(open('test.txt', 'rb').read())
+	print(loads(open('test.txt', 'rb').read()))
+	#M = loads(open('test.txt', 'rb').read()); import IPython; IPython.embed()
 
 
 	print(time.time() - tic)
