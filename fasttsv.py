e9c8eb346a20d83e773b80a1cf935071a21729d3
Vadim Kantorov <vadimkantorov@gmail.com>
2019-06-07 14:15:34 +0200

    ...

--- fasttsv.py
+++ fasttsv.py
@@ -1,6 +1,7 @@
 import io
 import numpy as np
 import time
+from numpy.lib.stride_tricks import as_strided
 
 def loads(b, max_integer_width = 4, encoding = 'utf-8', delimiter = '\t', newline = '\n', comments = '#'):
 	assert b[-1] == ord(newline)
@@ -15,6 +16,7 @@ def loads(b, max_integer_width = 4, encoding = 'utf-8', delimiter = '\t', newlin
 	floats = np.array(list(zip(*float_cols))[0]) if not uniform else float_cols
 
 	a = np.frombuffer(b, dtype = np.uint8)
+
 	newlines = a == ord(newline)
 	if has_names:
 		idx = np.flatnonzero(newlines)[0] + 1
@@ -22,33 +24,36 @@ def loads(b, max_integer_width = 4, encoding = 'utf-8', delimiter = '\t', newlin
 		newlines = newlines[idx:]
 
 	tabs = a == ord(delimiter)
-	points = a == ord(b'.')
 	minus = a == ord(b'-')
 	num_rows = newlines.sum()
 
+	a0 = np.zeros((max_integer_width - 1 + len(a), ), dtype = np.uint8)
+	np.subtract(a, ord(b'0'), out = a0[max_integer_width - 1:])
+	m = as_strided(a0, shape = [max_integer_width, len(a)], strides = a0.strides * 2)[::-1]
+	m = m * np.power(10, np.arange(max_integer_width), dtype = np.int32)[:, None]
+	for i in range(1, max_integer_width):
+		np.add(m[i], m[i - 1], out = m[i])
+
 	breaksi = np.flatnonzero(tabs | newlines)
-	breaksf = np.flatnonzero(points)
 
-	ai = (a - ord(b'0')).astype(np.int32)
-	m = [ai]
-	for p in range(1, max_integer_width):
-		m.append(m[-1] + 10 * np.pad(ai[:-p], (p, 0), mode = 'constant'))
-	m = np.vstack(m)
+	if len(integer_cols) > 0:
+		widthi = np.diff(np.pad(breaksi, (1, 0), mode = 'constant', constant_values = -1))
+		BI = (breaksi.reshape(num_rows, -1) - 1)[:, integers].flatten()
+		WI = (widthi.reshape(num_rows, -1) - 2)[:, integers].flatten()
+		resi = m[WI, BI].reshape(num_rows, -1)
 
-	widthi = np.diff(np.pad(breaksi, (1, 0), mode = 'constant', constant_values = -1))
-	BI = (breaksi.reshape(num_rows, -1) - 1)[:, integers].flatten()
-	WI = (widthi.reshape(num_rows, -1) - 2)[:, integers].flatten()
-	resi = m[WI, BI].reshape(num_rows, -1)
-
-	BF = (breaksi.reshape(num_rows, -1) - 1)[:, floats].flatten()
-	BF_ = np.vstack([breaksf - 1, BF]).T.flatten()
-	BF__ = (breaksi.reshape(num_rows, -1))[:, floats - 1].flatten()
-	widthf = BF - breaksf
-	WF_ = np.vstack([breaksf - BF__ - 1, widthf]).T.flatten() - 1
-	resf = m[WF_, BF_].reshape(num_rows, -1)
-	resf = np.ascontiguousarray(resf.astype(np.float32).reshape(-1, 2).T)
-	np.multiply(resf[1], np.power(10.0, -widthf, dtype = np.float32), out = resf[1])
-	resf = np.add(resf[0], resf[1], out = resf[1]).reshape(num_rows, -1)
+	if len(floats) > 0:
+		points = a == ord(b'.')
+		breaksf = np.flatnonzero(points)
+		BF = (breaksi.reshape(num_rows, -1) - 1)[:, floats].flatten()
+		BF_ = np.vstack([breaksf - 1, BF]).T.flatten()
+		BF__ = (breaksi.reshape(num_rows, -1))[:, floats - 1].flatten()
+		widthf = BF - breaksf
+		WF_ = np.vstack([breaksf - BF__ - 1, widthf]).T.flatten() - 1
+		resf = m[WF_, BF_].reshape(num_rows, -1)
+		resf = np.ascontiguousarray(resf.astype(np.float32).reshape(-1, 2).T)
+		np.multiply(resf[1], np.power(10.0, -widthf, dtype = np.float32), out = resf[1])
+		resf = np.add(resf[0], resf[1], out = resf[1]).reshape(num_rows, -1)
 
 	if not uniform:
 		integer_cols = {n : j for j, (i, n) in enumerate(integer_cols)}
@@ -58,10 +63,10 @@ def loads(b, max_integer_width = 4, encoding = 'utf-8', delimiter = '\t', newlin
 		return resi if len(integers) > 0 else resf
 		
 if __name__ == '__main__':
-	# run as python3 -m cProfile -s tottime tsv.py
+	# python3 -m cProfile -s tottime tsv.py
 
 	tic = time.time()
-	#print(loads(b'# a\tb\tc\td\n1\t22.60\t3\t5.0\n3\t44.8\t8\t9.09\n').dtype)
+	#print(loads(b'# a\tb\tc\td\n1\t22.60\t3\t5.0\n3\t44.8\t8\t9.09\n'))
 
 	#print(tsvparse(b'1\t22\n3\t44\n', integers = True))
 	#np.savetxt('test.txt', np.random.randint(0, 10000, size = (100000, 20)),fmt = '%d', delimiter = '\t')
@@ -71,5 +76,7 @@ if __name__ == '__main__':
 	#res = np.loadtxt(io.StringIO(b.decode('ascii')), dtype = np.float32 if floats else int, delimiter = '\t')
 
 	print(loads(open('test.txt', 'rb').read()).shape)
+	#M = loads(open('test.txt', 'rb').read())
+
 
 	print(time.time() - tic)
